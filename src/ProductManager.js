d2a64549a60fa7873038854cfbad844ab796b5f9
Lucas_Saidman <saidmanlucas.p@gmail.com>
2024-03-13 19:56:56 -0300

    Desafio 3 terminado

--- src/ProductManager.js
+++ src/ProductManager.js
@@ -1,13 +1,8 @@
-//En mi caso fue necesario instalar "npm install prompt-sync" en la consola para que funcione correctamente por que la consola me decia el siguiente error: "Cannot find module 'prompt-sync'"
-
 const fs = require("fs");
-
-//! Solución compartida por un compañero, encontrada en "https://www.codecademy.com/article/getting-user-input-in-node-js".
-const prompt = require('prompt-sync')();
 
 class ProductManager {
 
-    static id = 0;
+    static id = 10;
 
     constructor() {
 
@@ -22,8 +17,13 @@ class ProductManager {
             
             if(fs.existsSync(this.path)){
 
-                const readProductctsFromJSON = await fs.readFile(this.path, 'utf-8');
-                this.products = JSON.parse(readProductctsFromJSON);
+                const readProductsFromJSON = await fs.readFile(this.path, 'utf-8');
+                this.products = JSON.parse(readProductsFromJSON);
+
+                if (this.products.length > 0) {
+                    const maxId = Math.max(...this.products.map(product => product.id));
+                    ProductManager.id = maxId + 1;
+                }
 
             } else {
 
@@ -39,6 +39,8 @@ class ProductManager {
     }
 
     async addProduct(title, description, price, thumbnail, code, stock) {
+
+        ProductManager.id++;
 
         //Leo el archivo JSON
         const readProductsJSON = await fs.promises.readFile(this.path, 'utf-8');
@@ -62,7 +64,7 @@ class ProductManager {
         } else {
 
             const newProduct = {
-                id: ProductManager.id++,
+                id: ProductManager.id,
                 title,
                 description,
                 price,
@@ -146,64 +148,30 @@ class ProductManager {
 
     }
 
-    async updateProduct(id) {
+    async updateProduct(id, caracteristica, valor) {
 
-        const updateProduct = await this.getProductById(id);
-        console.log("Producto entontrado: ", updateProduct);
+        try {
 
-        console.log('Menu Update: ', '\n',
-              "1 - Titulo", '\n',
-              "2 - Descripción", '\n',
-              "3 - Precio", '\n',
-              "4 - Imagen", '\n',
-              "5 - Codigo", '\n',
-              "6 - Stock", '\n'
-        )
+            let colecciones = JSON.parse(await fs.promises.readFile(this.path, "utf-8"));
+            let productIndex = colecciones.findIndex((i)=> i.id == id);
 
-        const option = parseInt(prompt('Ingrese una option: '));
+            if (productIndex === -1) {
 
-        switch (option) {
+                return console.log(`Producto no encontrado ${id}`);
 
-            case 1:
-               updateProduct.title = prompt('Ingrese el nuevo titulo: ');
-               break;
-            case 2:
-                updateProduct.description = prompt('Ingrese nueva descripción: ');
-                break;
-            case 3:
-                updateProduct.price = parseInt(prompt('Ingrese nuevo precio: '));
-                break;
-            case 4:
-                updateProduct.thumbnail = prompt('Ingrese nueva ruta de imagen: ');
-                break;
-            case 5:
-                updateProduct.code = prompt('Ingrese nuevo código: ');
-                break;
-            case 6:
-                updateProduct.stock = parseInt(prompt('Ingrese nuevo stock: '));
-                break;
-            default:
-                console.log(`Opción no valida ${option}. Saliendo del programa...`)
-                return 'Opción no valida';
+            }
+    
+            colecciones[productIndex][caracteristica] = valor;
+            await fs.promises.writeFile(this.path, JSON.stringify(colecciones, null, 2));
+            this.products = colecciones;
+            console.log(`Producto ${id} editado`);
+
+        } catch (error) {
+
+            console.log('Error al actualizar el producto:', error.message);
 
         }
-
-        console.log('Producto actualizado: ', updateProduct);
-        this.products = await this.getProducts();
-
-        function replaceObjectById(array, IdObject, newObject) {
-            let index = array.findIndex(object => object.id === IdObject);
-            if(index !== -1) {
-                array[index] = newObject;
-                return array;
-            }
-         }
-
-        replaceObjectById(this.products, id, updateProduct);
-        await fs.promises.writeFile(this.path, JSON.stringify(this.products, null, 2));
-
-        console.log('Nuevo Array ', this.products);
-
+        
     }
 
     async deleteProduct(id) {
@@ -235,21 +203,17 @@ class ProductManager {
 
 }
 
+const producto = new ProductManager();
 
 
-//const producto = new ProductManager();
-
-
-//producto.addProduct("Procesador", 'R7 5700X', 350, "https://www.fullh4rd.com.ar/img/productos/1/micro-amd-ryzen-7-5700x-svideo-scooler-0.jpg", "R7-5", 20);
-//producto.addProduct("RTX 4090", ''RTX 4090 Asus Rog Strix', 250, "https://spacegamer.com.ar/img/Public/1058/55986-producto-1.jpg", "RTX4090-A", 10);
-//producto.addProduct("Monitor", 'Aorus CV27F', 7500, "https://cdn.mos.cms.futurecdn.net/WZhmwyBrt6LtrBzCdeyBuG.jpg", "CV27F", 5);
-//producto.addProduct("Notebook", 'Acer Predator Helios 300', 11500, "https://i.ytimg.com/vi/GC-xa9pl6Ao/maxresdefault.jpg", "APH300", 3);
+//producto.addProduct("Teclado", 'Logitech G413 Carbon', 55000, "https://d2r9epyceweg5n.cloudfront.net/stores/001/715/976/products/teclado-logitech1-a0d0f03734ef17c46216506377238422-1024-1024.jpg", "G413", 15);
+//producto.addProduct("Microfono", 'HyperX Pulsefire Surge', 32000, "https://cellplay.com.ar/img/Public/producto-137331-0.jpg", "HX-PS", 15);
 
 
 //producto.getProducts();
 //producto.getProductById(4);
-//producto.updateProduct(2);
-//producto.deleteProduct(0);
+//producto.updateProduct(10, "title", "PC");
+//producto.deleteProduct(12);
 
 
 
